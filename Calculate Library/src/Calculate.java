2ce6ec98c674ffb636d0ffdefaef81ce67864e79
RyanWong1 <18wongr29@millshighsch.org>
2016-08-31 10:47:35 -0700

    finsihed square 8/31

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,38 @@
+
+public class Calculate {
+
+	public static int square(int par){
+		int x=par*par;
+		return x;
+	}
+	
+	public static int cube(int par){
+		int x=par*par*par;
+		return x;
+	}
+	
+	public static double average(double par,double par2){
+		double x=(par+par2)/2;
+		return x;
+	}
+	
+	public static double average(double par1,double par2,double par3){
+		double x=(par1+par2+par3)/3;
+		return x;
+	}
+	
+	public static double toDegrees(double par){
+		double x=(par*180)/3.14159;
+		return x;
+	}
+	
+	public static double toRadians(double par){
+		double x=(par*3.14159)/180;
+		return x;
+	}
+	
+	public static int discriminant(int par){ //not finished
+		int x=par*par;
+		return x;
+	}
+}
