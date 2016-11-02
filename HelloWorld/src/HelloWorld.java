ed3a17ef7becad29d0059f92231da1caac88b447
RyanWong1 <18wongr29@millshighsch.org>
2016-11-02 10:44:52 -0700

    kugyiuguktd

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,9 @@
 
 public class HelloWorld {
 	public static void main(String[]args){
-		System.out.println("Hello World!");
+		System.out.println(test());
+	}
+	public static int test(){
+		return 2*(3+4);
 	}
 }
